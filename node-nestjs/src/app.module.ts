6aa86ee60955bb33391e71e8a0441373d6e496a4
LLAP <ospiper@foxmail.com>
2019-08-04 03:12:12 +0800

    Update app.module.ts
    deprecate ormconfig

--- node-nestjs/src/app.module.ts
+++ node-nestjs/src/app.module.ts
@@ -3,11 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Country } from './entity/country.entity';
+import { User } from './entity/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
-    TypeOrmModule.forFeature([Country]),
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'container.ll-ap.cn',
+        port: 5432,
+        username: 'postgres',
+        password: '',
+        database: 'benchmark',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        extra: {
+          max: 15,
+          idleTimeoutMillis: 30000,
+          connectionTimeoutMillis: 15000
+        },
+      },
+    ),
+    TypeOrmModule.forFeature([Country, User]),
   ],
   controllers: [AppController],
   providers: [AppService],
